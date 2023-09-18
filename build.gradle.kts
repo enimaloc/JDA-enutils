feedb75bdb06f5ffe671c0429ac11300f1e13b6d
Antoine <antoine@enimaloc.fr>
2023-09-18 04:19:13 +0200

    init: initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("java")
+}
+
+group = "fr.enimaloc"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("net.dv8tion:JDA:5.0.0-beta.13") {
+        exclude("club.minnced", "opus-java")
+    }
+
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
