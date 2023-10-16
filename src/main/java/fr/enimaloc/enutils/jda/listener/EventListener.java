58f6b4bbacd9901b4f4540efb0851f93c57524b8
Antoine <antoine@enimaloc.fr>
2023-10-16 09:46:26 +0200

    feat(threaded): use thread on executor and add thread factory

--- src/main/java/fr/enimaloc/enutils/jda/listener/EventListener.java
+++ src/main/java/fr/enimaloc/enutils/jda/listener/EventListener.java
@@ -1,18 +1,21 @@
 package fr.enimaloc.enutils.jda.listener;
 
+import fr.enimaloc.enutils.jda.Constant;
 import fr.enimaloc.enutils.jda.eventListener.Priority;
 import fr.enimaloc.enutils.jda.eventListener.RegisteredEvent;
-import fr.enimaloc.enutils.jda.exception.RegisteredExceptionHandler;
 import net.dv8tion.jda.api.events.GenericEvent;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.concurrent.ThreadFactory;
 
 public class EventListener implements net.dv8tion.jda.api.hooks.EventListener {
 
+    private final ThreadFactory threadFactory;
     private final List<RegisteredEvent> listeners;
 
-    public EventListener(List<RegisteredEvent> listeners) {
+    public EventListener(ThreadFactory threadFactory, List<RegisteredEvent> listeners) {
+        this.threadFactory = threadFactory;
         this.listeners = listeners;
     }
 
@@ -22,9 +25,13 @@ public class EventListener implements net.dv8tion.jda.api.hooks.EventListener {
                 .filter(e -> event.getClass().isAssignableFrom(e.event()))
                 .sorted(Comparator.<RegisteredEvent, Priority>comparing(RegisteredEvent::priority).reversed())
                 .toList()) {
-            if (registeredEvent.execute(event)) {
-                break;
-            }
+            Thread thread = threadFactory.newThread(() -> registeredEvent.execute(event));
+            thread.setName(Constant.ThreadName.EVENT_LISTENER_EXECUTOR.formatted(
+                    event.getClass().getSimpleName(),
+                    event.getJDA().getShardInfo().getShardId(),
+                    event.getJDA().getShardInfo().getShardTotal()
+            ));
+            thread.start();
         }
     }
 }
